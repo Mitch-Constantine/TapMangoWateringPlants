ae9801d7ae11bc3ddf19a1bf72cfb2ea4d50a007
Mitch-Constantine <Mitch.Constantine@gmail.com>
2020-01-16 12:29:29 -0500

    First cut of the UI

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -2,7 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    plants = [
+        {
+            plantName: "Plant 1",
+            isWateringNow : false
+        },
+        {
+            plantName: "Plant 2",
+            isWateringNow : false
+        },
+        {
+            plantName: "Plant 3",
+            isWateringNow : false
+        }
+    ]
+
+    setWateringStatus(i: number, status: boolean) {
+        this.plants[i].isWateringNow = status;
+    }
 }
